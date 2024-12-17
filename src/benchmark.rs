c212db2ee8a4a142dee5fbbc492265329ab10e8d
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-12-17 18:36:46 +0100

    Run benchmarks and export CSV. To be cleaned up

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -9,10 +9,15 @@ use macos::sampler::Sampler;
 use chrono::{Duration, Utc};
 use core::fmt;
 use std::process::Command;
+use std::fs::{self};
+use std::io;
+use serde::Serialize;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize)]
 pub struct Export {
-    duration: Duration,
+    language: String,
+    task: String,
+    duration: i64,
     energy: f64,
 }
 
@@ -22,13 +27,14 @@ impl fmt::Display for Export {
             f,
             "{} µj, {} ms, {:.5} w",
             self.energy,
-            self.duration.num_milliseconds(),
-            self.energy / self.duration.num_microseconds().expect("bull") as f64
+            self.duration,
+            self.energy / self.duration as f64
         )
     }
 }
 
-pub struct Exports(Vec<Export>);
+#[derive(Serialize)]
+pub struct Exports(pub Vec<Export>);
 
 impl fmt::Display for Exports {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -36,23 +42,76 @@ impl fmt::Display for Exports {
         for (i, v) in self.0.iter().enumerate() {
             write!(
                 f,
-                "{:>2} - {:>10} µj, {:>3} ms, {:>10.5} w\n",
+                "{} - {} - {:>2} - {:>10} µj, {:>3} ms, {:>10.5} w\n",
+                v.language,
+                v.task,
                 i,
                 v.energy,
-                v.duration.num_milliseconds(),
-                v.energy as f32 / v.duration.num_microseconds().expect("bull") as f32
+                v.duration,
+                v.energy / v.duration as f64
             )?;
         }
         Ok(())
     }
 }
 
-pub fn benchmark(mut cmd: Command, runs: u64) -> Exports {
+pub fn run(path: &str, runs: u64) -> io::Result<Vec<Exports>> {
+    let mut res: Vec<Exports> = vec![];
+
+    // For each language
+    for lang in fs::read_dir(path)? {
+        let lang = lang?;
+        let language_path = lang.path();
+
+        // Skip files found
+        if !language_path.is_dir() {
+            continue;
+        }
+
+        // For each task
+        for task in fs::read_dir(&language_path)? {
+            let task = task?;
+            let task_path = task.path();
+
+            /*
+                For a faster runtime
+                if let Some(x) = task_path.to_str() {
+                    if !x.contains("C") {
+                        continue; 
+                }
+            }*/
+
+            // Skip files found
+            if !task_path.is_dir() {
+                continue
+            }
+
+            if let Some(str) = task.path().to_str() {
+                // Get language name and Task
+                let parts: Vec<&str> = str.split("/").collect();
+
+                // Create make command
+                let mut cmd = Command::new("make");
+                cmd.arg("-C").arg(str).arg("run");
+
+                // Run benchmark
+                res.push(benchmark(cmd, runs, parts[3],parts[4]));
+            }
+        }
+    }
+
+    Ok(res)
+}
+
+pub fn benchmark(mut cmd: Command, runs: u64, lang: &str, task: &str) -> Exports {
     let sampler = Sampler::new();
 
     let mut exports: Vec<Export> = vec![];
 
-    for _n in 0..runs {
+    for n in 0..runs {
+
+        println!("Running {} / {} - {}/{}", lang, task, n+1, runs);
+
         let start_time = Utc::now().time();
         let start = sampler.sample_start();
 
@@ -63,7 +122,11 @@ pub fn benchmark(mut cmd: Command, runs: u64) -> Exports {
 
         let energy = sampler.sample_end(start);
         let duration = Utc::now().time() - start_time;
-        exports.push(Export { duration, energy });
+        exports.push(Export { 
+            language: lang.to_string(), 
+            task: task.to_string(), 
+            duration: duration.num_milliseconds(), 
+            energy });
     }
 
     Exports(exports)
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,49 @@
 mod benchmark;
 
+use benchmark::Exports;
 use clap::Parser;
-use std::process::Command;
-
-use benchmark::{benchmark, summarize};
+use std::{io::Write, process::Command};
+use csv::Writer;
 
 #[derive(Debug, Parser)]
 #[command(version, verbatim_doc_comment)]
 struct CLI {
     #[arg(short, long, default_value_t = 11)]
     runs: u64,
-    #[arg(short, long, default_value = "pwd")]
-    command: String,
 }
 
 fn main() {
     let args = CLI::parse();
 
-    let exports = benchmark(generate_command(&args.command), args.runs);
-    println!("{}", &exports);
-    println!("Totals: {}", summarize(exports));
+
+    let exports = match benchmark::run("../../benchmarks", args.runs) {
+        Ok(exp) => exp,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+
+    let _ = csv(exports);
+}
+
+use std::fs::File;
+fn csv(data: Vec<Exports>) -> Result<(), Box<dyn std::error::Error>> {
+
+    // Serialize to CSV
+    let mut writer = Writer::from_writer(vec![]);
+    for lang in data {
+        for itt in lang.0 {
+            writer.serialize(itt)?;
+        }
+    }
+
+    // Write data to CSV
+    let data = String::from_utf8(writer.into_inner()?)?;
+    let mut file = File::create("energy.csv")?;
+    file.write_all(data.as_bytes())?;
+   
+    Ok(())
 }
 
 fn generate_command(command: &str) -> Command {
